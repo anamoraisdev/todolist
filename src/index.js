d34bf1f62fbc368c53479f619e2ac9b7de2e4074
anamoraisdev <anamorais.dev@gmail.com>
2022-11-30 12:15:57 -0300

    criando arquivos

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import List from './components/list';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <List/>
+  </React.StrictMode>
+);
+
+
